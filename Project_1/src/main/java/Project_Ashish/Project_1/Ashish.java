92757d241a441e506ff66d7efdddf4e8aacc1848
ASHISH <ASHISH@ASHISH-PC>
2019-07-18 23:17:31 +0530

    Secnod Commit

---
+++ Project_1/src/main/java/Project_Ashish/Project_1/Ashish.java
@@ -0,0 +1,11 @@
+package Project_Ashish.Project_1;
+
+public class Ashish {
+	
+	public static void main (String args[])
+	{
+		System.out.println("*****************************************");
+		System.out.println("*****************************************");
+	}
+
+}
